533e9ad03f56be1a612307c23dc8318ae50cb1cf
N.Budyaev <54439506+n-budyaev@users.noreply.github.com>
2022-03-24 11:59:18 +0400

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,6 +1,6 @@
 import time
-#print('О чем напомнить?')
-mes = str(input('О чем .напомнить?'))
+
+mes = str(input('О чем напомнить?'))
 print('Через сколько минут?')
 local_time = float(input())
 local_time = local_time * 60
